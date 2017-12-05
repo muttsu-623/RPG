c30bb425cee38710bdf9b7ff15d9bd44e586088d
muttsu-623 <mutsumi1996@gmail.com>
2017-12-05 18:25:30 +0000

    Heroクラスのh2と、Wizardクラスのwを作成し、フィールドにそれぞれ値を代入

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         // Heroクラスのhを作成
-        Hero h = new Hero();
-        h.name = "ミナト";
-        h.hp = "100";
+        Hero h1 = new Hero();
+        h1.name = "ミナト";
+        h1.hp = 100;
+
+        // Heroクラスのh2を作成し、フィールドに値を代入
+        Hero h2 = new Hero();
+        h2.name = "アサカ";
+        h2.hp = 150;
+
+        // Wizardクラスのwを作成し、フィールドに値を代入
+        Wizard w = new Wizard();
+        w.name = "スガワラ";
+        w.hp = 100;
 
         // Matangoクラスのm1を作成し、フィールドに値を代入
         Matango m1 = new Matango();
