f67d2b2a68766d294d9f48f71a33729ec0689eb4
muttsu-623 <mutsumi1996@gmail.com>
2017-12-05 17:41:23 +0000

    メインクラスの作成

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Heroクラスのhを作成
+        Hero h = new Hero();
+        h.name = "ミナト";
+        h.hp = "100";
+
+        // Matangoクラスのm1を作成し、フィールドに値を代入
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        // Matangoクラスのm2を作成し、フィールドに値を代入
+        Matango m2 = new Matango();
+        m2.hp = 40;
+        m2.suffix = 'B';
+
+        //　冒険の始まり
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
     }
+
 }
